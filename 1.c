a7b47259733675ce08652327cb2ec21d65f75ca9
skyline75489 <skyline75489@outlook.com>
2014-10-04 10:52:30 +0800

    add comment

--- 1.c
+++ 1.c
@@ -5,13 +5,12 @@ double _my_double1 = 16.78;
 for(int i=0;i<10;i++) {
     i += 10;
 }
-char *a = "Hello";
+char *a = "Hello;
 int c = 10;
 while(c--) {
     a += (b - c) / 2;
 }
 
-/**/ 
 if (a != b) {
    c = a + b
 }
