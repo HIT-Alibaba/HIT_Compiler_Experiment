2b9cff6364c8c8da2296e2d61ef8c7eb31761435
skyline75489 <skyline75489@outlook.com>
2014-10-08 10:20:54 +0800

    autopep8

--- lexer.py
+++ lexer.py
@@ -4,57 +4,67 @@ import sys
 
 KEYWORD_LIST = ['if', 'else', 'while', 'break', 'continue', 'for', 'double', 'int', 'float', 'long', 'short',
                 'switch', 'case', 'return']
-        
-SEPARATOR_LIST = ['{', '}', '[', ']', '(', ')', '~', ',', ';', '.', '#', '?', ':']
 
-OPERATOR_LIST = ['+', '++', '-', '--', '+=', '-=', '*', '*=', '/', '/=', '>', '<', '>=', '<=', '=', '==', '!=', '!']
+SEPARATOR_LIST = [
+    '{', '}', '[', ']', '(', ')', '~', ',', ';', '.', '#', '?', ':']
+
+OPERATOR_LIST = ['+', '++', '-', '--', '+=', '-=', '*', '*=',
+                 '/', '/=', '>', '<', '>=', '<=', '=', '==', '!=', '!']
 
 current_pos = -1
 current_line = 0
 input_str = []
 
+
 def is_keyword(s):
     return s in KEYWORD_LIST
-    
+
+
 def is_separator(s):
     return s in SEPARATOR_LIST
-    
+
+
 def is_operator(s):
     return s in OPERATOR_LIST
-    
+
+
 def getchar():
     global current_pos
     global current_line
     current_pos = current_pos + 1
     if current_pos == len(input_str[current_line]):
         current_line = current_line + 1
-        current_pos = 0;
+        current_pos = 0
 
     if current_line == len(input_str):
         return 'SCANEOF'
     return input_str[current_line][current_pos]
-    
+
+
 def ungetc():
     global current_pos
     global current_line
     current_pos = current_pos - 1
     if current_pos < 0:
-        current_line = current_line - 1;
-        current_pos = 0;
+        current_line = current_line - 1
+        current_pos = 0
     return input_str[current_line][current_pos]
-    
+
+
 def read_file(file):
     global input_str
-    f = open(file, 'r');
-    input_str = f.readlines();
+    f = open(file, 'r')
+    input_str = f.readlines()
     f.close()
-    
+
+
 def lexical_error(info, line=None, row=None):
     if line is None:
         line = current_line + 1
     if row is None:
         row = current_pos + 1
     print(str(line) + ':' + str(row) + ' Lexical error: ' + info)
+
 
 def scanner():
     current_char = getchar()
@@ -67,11 +77,11 @@ def scanner():
         while current_char.isdigit():
             int_value = int_value * 10 + int(current_char)
             current_char = getchar()
-        
+
         if current_char != '.':
             ungetc()
             return ('INT', int_value)
-        
+
         float_value = str(int_value) + '.'
         current_char = getchar()
         while current_char.isdigit():
@@ -89,14 +99,14 @@ def scanner():
             return ('KEYWORD', string)
         else:
             return ('ID', string)
-            
+
     if current_char == '\"':
         str_literal = ''
         global current_line
         global current_pos
         line = current_line + 1
         row = current_pos + 1
-        
+
         current_char = getchar()
         while current_char != '\"':
             str_literal += current_char
@@ -108,11 +118,11 @@ def scanner():
                 current_pos = row
                 return 'SCANEOF'
         return('STRING', str_literal)
-    
+
     if current_char == '/':
         next_char = getchar()
-        line = int(current_line) + 1;
-        row = int(current_pos) + 1;
+        line = int(current_line) + 1
+        row = int(current_pos) + 1
         if next_char == '*':
             comment = ''
             next_char = getchar()
@@ -137,10 +147,10 @@ def scanner():
                 current_char = getchar()
             ungetc()
             return ('OP', op)
-            
+
     if is_separator(current_char):
         return ('SEP', current_char)
-    
+
     if is_operator(current_char):
         op = ''
         while is_operator(current_char):
@@ -151,7 +161,8 @@ def scanner():
     else:
         print(current_char)
         lexical_error('unknown character')
-            
+
+
 def main():
     global input_str
     global current_pos
@@ -161,11 +172,9 @@ def main():
     while True:
         r = scanner()
         if r == 'SCANEOF':
-            break;
+            break
         if r is not None:
             print(r)
-        
+
 if __name__ == '__main__':
     main()
-    
-    
